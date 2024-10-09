b4c08ea046a9aa309f8994b0923e7f6f2f1ebb06
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-09 16:25:36 -0400

    2-INIT version-1.1

--- servicio-unidades/src/main/java/com/galileo/cu/unidades/ServicioUnidadesApplication.java
+++ servicio-unidades/src/main/java/com/galileo/cu/unidades/ServicioUnidadesApplication.java
@@ -20,7 +20,7 @@ public class ServicioUnidadesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("UNIDADES V1.1-2024-10-09 14:29");
+		System.out.println("UNIDADES V1.1-2024-10-09 16:25");
 	}
 
 }
